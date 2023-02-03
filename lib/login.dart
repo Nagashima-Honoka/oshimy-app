896ec01ae70ba9eac1dee764acf2ed3ab1f485a3
Nozomi Miyake <hm.te.n.ngus@gmail.com>
2023-02-03 15:19:02 +0900

    FirebaseにFlutterアプリを連携

---
+++ lib/login.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const LoginPage());
+// }
+//
+// class MyTodoApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // アプリ名
+//       title: 'My Todo App',
+//       theme: ThemeData(
+//         // テーマカラー
+//         primarySwatch: Colors.blue,
+//       ),
+//       // リスト一覧画面を表示
+//       home: TodoListPage(),
+//     );
+//   }
+// }
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
